1695aa0bb2284385487855a5c6929c8d4bdf572f
igao <igorsouzafss@gmail.com>
2023-10-26 00:40:45 -0300

    chore: initial configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,35 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      success: {
+        100: '#d4edda',
+        200: '#28a745',
+        400: '#218838'
       },
+      error: {
+        100: '#f8d7da',
+        200: '#dc3545',
+        400: '#a71d2a'
+      },
+      warning: {
+        100: '#fff3cd',
+        200: '#ffc107',
+        400: '#d39e00'
+      },
+      gray: {
+        100: '#FBFBFB',
+        200: '#EAEAEA',
+        300: '#D3D3D3',
+        400: '#BDBDBD',
+        600: '#707070'
+      }
     },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
 export default config
