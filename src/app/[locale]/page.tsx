117c8a4b490edcdef2f02e6407237eaf9a7c4f6c
igao <igorsouzafss@gmail.com>
2023-11-01 18:38:02 -0300

    feat: in development

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,13 @@
 import { useTranslations } from 'next-intl'
+import Image from 'next/image'
+import logo from '../../../public/images/svg/logo.svg'
 
 export default function Home() {
   const t = useTranslations('Home')
   return (
-    <main className="font-kern flex min-h-screen flex-col items-center justify-between p-24 font-bold">
-      <h1>{t('title')}</h1>
+    <main className="flex min-h-screen flex-col items-center gap-6 bg-[#FF4917] p-24 font-kern font-bold">
+      <Image src={logo} width={338.4658} height={57.368} alt="Logo Ana Couto" />
+      <h1 className="text-3xl font-normal text-gray-100">{t('title')}</h1>
     </main>
   )
 }
