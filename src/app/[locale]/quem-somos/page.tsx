9cbedf43a722ce5cb1d1623982ed249600c5b0b3
igao <igorsouzafss@gmail.com>
2023-11-02 13:19:58 -0300

    feat: first section of the who we are page ok

--- src/app/[locale]/quem-somos/page.tsx
+++ src/app/[locale]/quem-somos/page.tsx
@@ -1,10 +1,20 @@
+import { useTranslations } from 'next-intl'
 import { Fragment } from 'react'
 
-export default async function WhoWeAre() {
+export default function WhoWeAre() {
+  const t = useTranslations('Contact.firstSection')
+
   return (
     <Fragment>
-      <h1>who we are :p</h1>
-      <h1>who we are :p</h1>
+      <section className="flex w-full flex-col gap-10 px-20 py-10">
+        <h1 className="w-3/5 font-kernCompressed text-8xl font-semibold">
+          {t('title')}
+        </h1>
+
+        <p className="w-3/12 self-end text-2xl">{t('subtitle')}</p>
+      </section>
+
+      <p>Oi</p>
     </Fragment>
   )
 }
