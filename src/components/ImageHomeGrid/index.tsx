1292ca033a355576f6f7c0020e9cd6247e09a827
igao <igorsouzafss@gmail.com>
2023-11-12 15:19:59 -0300

    fix: padding imagens gallery home

--- src/components/ImageHomeGrid/index.tsx
+++ src/components/ImageHomeGrid/index.tsx
@@ -3,151 +3,148 @@ import { cn } from '@/common/utils/cn'
 import useMediaQuery from '@/hooks/useMediaQuery'
 import Image from 'next/image'
 import { ImageGalleryHome } from '../ImageGalleryHome'
-import React from 'react'
 
 export const ImageHomeGrid = () => {
   const isMobile = useMediaQuery('(max-width: 600px)')
   return (
     <>
       {isMobile ? (
-        <>
-          <div className="relative h-auto w-full flex-col gap-1 px-4 ">
-            <div className="relative w-full">
-              <button
-                className={cn(
-                  'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
-                  'bg-white text-black'
-                )}
-              >
-                <text className="text-xs">Case</text>
-              </button>
-              <Image
-                src={'/images/banneronemob.png'}
-                alt="Imagem 1"
-                objectFit="cover"
-                width={1865}
-                height={814}
-                style={{ width: '100%' }}
-              />
-              <div className="absolute bottom-3 left-3 flex flex-col items-start gap-1 text-white">
-                <span className="text-base leading-3">Grupo Sabin</span>
-                <span className="text-xs leading-3">Propósito aqui</span>
-              </div>
-            </div>
-            <div className="my-1 flex flex-col justify-between gap-1">
-              <div className="relative w-full ">
-                <div className="relative mb-1 w-full">
-                  <button
-                    className={cn(
-                      'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
-                      'bg-white text-black'
-                    )}
-                  >
-                    <text className="text-xs">Case</text>
-                  </button>
-                  <Image
-                    src={'/images/bannertwomob.png'}
-                    alt="Imagem 2"
-                    objectFit="cover"
-                    width={918}
-                    height={919}
-                    style={{ width: '100%', height: '100%' }}
-                  />
-                  <div className="absolute bottom-5 left-5 flex flex-col items-start gap-1 text-white">
-                    <span className="text-base leading-3">Zuk</span>
-                    <span className="text-xs leading-3">
-                      O valor que o país gera.
-                    </span>
-                  </div>
-                </div>
-                <div className="relative w-full">
-                  <button
-                    className={cn(
-                      'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
-                      'bg-white text-black'
-                    )}
-                  >
-                    <text className="text-xs">Case</text>
-                  </button>
-                  <Image
-                    src={'/images/bannertreemob.png'}
-                    alt="Imagem 4"
-                    objectFit="cover"
-                    width={918}
-                    height={421}
-                    style={{ width: '100%' }}
-                  />
-                  <div
-                    className={cn(
-                      'absolute bottom-5 left-5 flex flex-col items-start gap-1',
-                      'text-white'
-                    )}
-                  >
-                    <span className="text-base leading-3">Cosan</span>
-                    <span className="text-xs leading-3">
-                      O valor que o país gera.
-                    </span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="relative w-full">
-                <div className="relative mb-1 w-full">
-                  <button
-                    className={cn(
-                      'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
-                      'bg-white text-black'
-                    )}
-                  >
-                    <text className="text-xs">Case</text>
-                  </button>
-                  <Image
-                    src={'/images/bannerfourmob.png'}
-                    alt="Imagem 3"
-                    objectFit="cover"
-                    width={918}
-                    height={421}
-                    style={{ width: '100%' }}
-                  />
-                  <div className="absolute bottom-3 left-3 flex flex-col items-start gap-1 text-white">
-                    <span className="text-base leading-3">Natura</span>
-                    <span className="text-xs leading-3">Tododia Todanoite</span>
-                  </div>
-                </div>
-                <div className="relative w-full">
-                  <button
-                    className={cn(
-                      'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
-                      'bg-white text-black'
-                    )}
-                  >
-                    <text className="text-xs">Case</text>
-                  </button>
-                  <Image
-                    src={'/images/bannerfivemob.png'}
-                    alt="Imagem 5"
-                    objectFit="cover"
-                    width={918}
-                    height={421}
-                    style={{ width: '100%' }}
-                  />
-                  <div className="absolute bottom-3 left-3 flex flex-col items-start gap-1 text-white">
-                    <span className="text-base leading-3">HappyEggs</span>
-                    <span className="text-xs leading-3">Texto aqui</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex justify-between gap-1 ">
-              <ImageGalleryHome />
+        <div className="relative h-auto w-full flex-col gap-1 px-1 sm:px-4">
+          <div className="relative w-full">
+            <button
+              className={cn(
+                'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
+                'bg-white text-black'
+              )}
+            >
+              <text className="text-xs">Case</text>
+            </button>
+            <Image
+              src={'/images/banneronemob.png'}
+              alt="Imagem 1"
+              objectFit="cover"
+              width={1865}
+              height={814}
+              style={{ width: '100%' }}
+            />
+            <div className="absolute bottom-3 left-3 flex flex-col items-start gap-1 text-white">
+              <span className="text-base leading-3">Grupo Sabin</span>
+              <span className="text-xs leading-3">Propósito aqui</span>
             </div>
           </div>
-        </>
+          <div className="my-1 flex flex-col justify-between gap-1">
+            <div className="relative w-full ">
+              <div className="relative mb-1 w-full">
+                <button
+                  className={cn(
+                    'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
+                    'bg-white text-black'
+                  )}
+                >
+                  <text className="text-xs">Case</text>
+                </button>
+                <Image
+                  src={'/images/bannertwomob.png'}
+                  alt="Imagem 2"
+                  objectFit="cover"
+                  width={918}
+                  height={919}
+                  style={{ width: '100%', height: '100%' }}
+                />
+                <div className="absolute bottom-5 left-5 flex flex-col items-start gap-1 text-white">
+                  <span className="text-base leading-3">Zuk</span>
+                  <span className="text-xs leading-3">
+                    O valor que o país gera.
+                  </span>
+                </div>
+              </div>
+              <div className="relative w-full">
+                <button
+                  className={cn(
+                    'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
+                    'bg-white text-black'
+                  )}
+                >
+                  <text className="text-xs">Case</text>
+                </button>
+                <Image
+                  src={'/images/bannertreemob.png'}
+                  alt="Imagem 4"
+                  objectFit="cover"
+                  width={918}
+                  height={421}
+                  style={{ width: '100%' }}
+                />
+                <div
+                  className={cn(
+                    'absolute bottom-5 left-5 flex flex-col items-start gap-1',
+                    'text-white'
+                  )}
+                >
+                  <span className="text-base leading-3">Cosan</span>
+                  <span className="text-xs leading-3">
+                    O valor que o país gera.
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="relative w-full">
+              <div className="relative mb-1 w-full">
+                <button
+                  className={cn(
+                    'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
+                    'bg-white text-black'
+                  )}
+                >
+                  <text className="text-xs">Case</text>
+                </button>
+                <Image
+                  src={'/images/bannerfourmob.png'}
+                  alt="Imagem 3"
+                  objectFit="cover"
+                  width={918}
+                  height={421}
+                  style={{ width: '100%' }}
+                />
+                <div className="absolute bottom-3 left-3 flex flex-col items-start gap-1 text-white">
+                  <span className="text-base leading-3">Natura</span>
+                  <span className="text-xs leading-3">Tododia Todanoite</span>
+                </div>
+              </div>
+              <div className="relative w-full">
+                <button
+                  className={cn(
+                    'absolute left-3 top-3 flex h-6 w-14 items-center justify-center rounded p-2',
+                    'bg-white text-black'
+                  )}
+                >
+                  <text className="text-xs">Case</text>
+                </button>
+                <Image
+                  src={'/images/bannerfivemob.png'}
+                  alt="Imagem 5"
+                  objectFit="cover"
+                  width={918}
+                  height={421}
+                  style={{ width: '100%' }}
+                />
+                <div className="absolute bottom-3 left-3 flex flex-col items-start gap-1 text-white">
+                  <span className="text-base leading-3">HappyEggs</span>
+                  <span className="text-xs leading-3">Texto aqui</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between gap-1 ">
+            <ImageGalleryHome />
+          </div>
+        </div>
       ) : (
         <>
           {' '}
-          <div className="relative h-auto w-full flex-col gap-3 px-7">
+          <div className="relative h-auto w-full flex-col gap-3 px-1 sm:px-4">
             <div className="relative w-full">
               <button
                 className={cn(
