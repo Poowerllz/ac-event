0e3a23a07ab6590d3ff7653c1832744b4d6c15f3
Poowerllz <safilhoelzevirsafilho@gmail.com>
2023-11-15 12:19:15 -0300

    feat: menu animation

--- src/components/Header/Menu/index.tsx
+++ src/components/Header/Menu/index.tsx
@@ -4,8 +4,9 @@ import logoAnaCouto from '@/images/logo.png'
 import iconCloseButon from '@/images/svg/times.svg'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import iconMenu from '../../../../public/menu.svg'
+import { motion, AnimatePresence } from 'framer-motion'
 
 type Props = {
   invertColor?: boolean
@@ -22,6 +23,14 @@ const Menu: React.FC<Props> = ({ invertColor }) => {
     { label: 'Contato', path: '/contato' }
   ]
 
+  useEffect(() => {
+    if (menuIsOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }, [menuIsOpen])
+
   return (
     <>
       <Image
@@ -37,66 +46,102 @@ const Menu: React.FC<Props> = ({ invertColor }) => {
         onClick={() => setMenuIsOpen(true)}
       />
 
-      {menuIsOpen && (
-        <div className="fixed left-0 top-0 z-50 flex h-screen w-screen flex-col overflow-auto bg-black p-6 sm:p-16">
-          <div className="flex w-full items-center justify-between">
-            <Link
-              href={'/'}
-              scroll={false}
-              onClick={() => setMenuIsOpen(false)}
+      <div className="absolute right-10 top-16 flex items-center justify-center sm:right-20">
+        <AnimatePresence>
+          {menuIsOpen && (
+            <motion.div
+              key={'header-modal'}
+              initial={{
+                width: '32px',
+                height: '32px',
+                borderRadius: '50%'
+              }}
+              animate={{
+                width: '4000px',
+                height: '4000px'
+              }}
+              exit={{
+                width: '32px',
+                height: '32px',
+                borderRadius: '50%'
+              }}
+              transition={{
+                ease: 'circIn',
+                duration: 0.5
+              }}
+              className="absolute z-50 flex h-screen w-screen flex-col overflow-hidden bg-black"
             >
-              <Image
-                src={logoAnaCouto}
-                alt={'Imagem da logo'}
-                className="w-16 sm:w-36"
-                quality={100}
-                height={150}
-                width={150}
-              />
-            </Link>
-
-            <div className="flex items-center gap-4 sm:gap-10">
-              <h2 className="text-md cursor-pointer font-kernCompressed font-normal text-white hover:text-primary sm:text-2xl">
-                Loja
-              </h2>
-              <h2 className="text-md cursor-pointer font-kernCompressed font-normal text-white hover:text-primary sm:text-2xl">
-                Valometry
-              </h2>
-              <h2 className="text-md cursor-pointer font-kernCompressed font-normal text-white hover:text-primary sm:text-2xl">
-                Carreiras
-              </h2>
-              <Image
-                onClick={() => setMenuIsOpen(false)}
-                src={iconCloseButon}
-                className="w-4 cursor-pointer sm:w-6"
-                alt={'Ícone para fechar'}
-                width={24}
-                height={24}
-              />
-            </div>
-          </div>
-
-          <div className="mt-12 flex w-full flex-col gap-4 sm:mt-24">
-            {items.map((item, idx) => (
-              <Link
-                key={idx}
-                href={item.path}
-                scroll={false}
-                onClick={() => setMenuIsOpen(false)}
+              <motion.div
+                initial={{ opacity: 0, left: '100px' }}
+                animate={{ opacity: 1, left: 0 }}
+                exit={{ opacity: 0, left: '100px', transition: { delay: 0 } }}
+                transition={{
+                  delay: 0.51
+                }}
+                key={'texts'}
+                className="fixed left-0 top-0 flex h-screen w-screen flex-col overflow-auto p-6 sm:p-16"
               >
-                <h3
-                  className={cn(
-                    'cursor-pointer font-kernCompressed text-6xl font-bold hover:text-primary sm:text-8xl',
-                    'text-off_white'
-                  )}
-                >
-                  {item.label}
-                </h3>
-              </Link>
-            ))}
-          </div>
-        </div>
-      )}
+                <div className="flex w-full items-center justify-between">
+                  <Link
+                    href={'/'}
+                    scroll={false}
+                    onClick={() => setMenuIsOpen(false)}
+                  >
+                    <Image
+                      src={logoAnaCouto}
+                      alt={'Imagem da logo'}
+                      className="w-16 sm:w-36"
+                      quality={100}
+                      height={150}
+                      width={150}
+                    />
+                  </Link>
+
+                  <div className="flex items-center gap-4 sm:gap-10">
+                    <h2 className="text-md cursor-pointer font-kernCompressed font-normal text-white hover:text-primary sm:text-2xl">
+                      Loja
+                    </h2>
+                    <h2 className="text-md cursor-pointer font-kernCompressed font-normal text-white hover:text-primary sm:text-2xl">
+                      Valometry
+                    </h2>
+                    <h2 className="text-md cursor-pointer font-kernCompressed font-normal text-white hover:text-primary sm:text-2xl">
+                      Carreiras
+                    </h2>
+                    <Image
+                      onClick={() => setMenuIsOpen(false)}
+                      src={iconCloseButon}
+                      className="w-4 cursor-pointer sm:w-6"
+                      alt={'Ícone para fechar'}
+                      width={24}
+                      height={24}
+                    />
+                  </div>
+                </div>
+
+                <div className="mt-12 flex w-full flex-col gap-4 sm:mt-24">
+                  {items.map((item, idx) => (
+                    <Link
+                      key={idx}
+                      href={item.path}
+                      scroll={false}
+                      onClick={() => setMenuIsOpen(false)}
+                    >
+                      <h3
+                        className={cn(
+                          'cursor-pointer font-kernCompressed text-6xl font-bold hover:text-primary sm:text-8xl',
+                          'text-off_white'
+                        )}
+                      >
+                        {item.label}
+                      </h3>
+                    </Link>
+                  ))}
+                </div>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
     </>
   )
 }
