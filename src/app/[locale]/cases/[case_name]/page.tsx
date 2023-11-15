6539c58fd2ede1f771a0d522111e658a23cce539
Poowerllz <safilhoelzevirsafilho@gmail.com>
2023-11-15 18:28:58 -0300

    feat: fix iframe logic

--- src/app/[locale]/cases/[case_name]/page.tsx
+++ src/app/[locale]/cases/[case_name]/page.tsx
@@ -1,14 +1,20 @@
+import IframeResizer from 'iframe-resizer-react'
+import { headers } from 'next/headers'
+
 export default function Page({ params }: { params: { case_name: string } }) {
   const { case_name } = params
+  const nonce = headers().get('x-nonce')
+
   return (
     <section className="min-h-screen w-full">
       <iframe
         src={`https://www.anacouto.com.br/cases/${case_name}/`}
         title={`${case_name} Case`}
-        width="100%"
-        height="600"
+        style={{
+          height: '100vh',
+          width: '100%'
+        }}
         frameBorder="0"
-        scrolling="no"
       />
     </section>
   )
