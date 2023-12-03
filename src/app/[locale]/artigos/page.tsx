ac3802ed68298ed3aa036de6b14c0b4fc6b4cb1e
igao <igorsouzafss@gmail.com>
2023-12-03 11:37:16 -0300

    fix: section margin in articles

--- src/app/[locale]/artigos/page.tsx
+++ src/app/[locale]/artigos/page.tsx
@@ -1,9 +1,9 @@
 import { ArticlesGalleryContainer } from '@/components/ArticlesGallery/ArticlesGalleryContainer'
 import { ArrowMobile } from '@/components/arrowMobile'
 import { Typography } from '@/components/ui/Typography'
+import ArrowTop from '@/images/svg/arrowtop.svg'
 import { useTranslations } from 'next-intl'
 import { Fragment } from 'react'
-import ArrowTop from '@/images/svg/arrowtop.svg'
 
 export default function Artigos() {
   const t = useTranslations('Articles')
@@ -11,10 +11,10 @@ export default function Artigos() {
   return (
     <Fragment>
       <section
-        className="flex h-screen w-full flex-col gap-10 px-6 py-10 sm:mb-40 sm:px-16"
+        className="flex w-full flex-col gap-10 px-6 py-10 sm:mb-40 sm:px-16"
         id={'twoSection'}
       >
-        <div className="flex h-screen w-full items-start justify-between">
+        <div className="flex w-full items-start justify-between">
           <div className="w-full sm:w-4/5 md:w-4/5 lg:w-4/6 xl:w-1/2">
             <Typography as="h1">{t('sectionMessage')}</Typography>
           </div>
