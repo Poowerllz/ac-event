918f64d89a37704d5de49b3818c434105b922d7e
Poowerllz <safilhoelzevirsafilho@gmail.com>
2023-12-13 23:17:55 -0300

    feat: integration with WP api

--- src/components/ArticlesGallery/AticlesHome.tsx
+++ src/components/ArticlesGallery/AticlesHome.tsx
@@ -5,53 +5,62 @@ import { toBase64 } from '@/common/utils/toBase64'
 import BackgroundPlaceholder from '@/images/png/background-placeholder.png'
 import Image from 'next/image'
 import Link from 'next/link'
-import { mockArticles } from '../ArticlesGalleryV2'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export function AticlesHome() {
   const divsClass = ['', '', 'hidden sm:flex', 'hidden sm:flex']
-  const posts = mockArticles.slice(0, 4)
+  const [posts, setPosts] = useState<any[]>()
+
+  useEffect(() => {
+    axios
+      .get('https://web.anacouto.com.br/?rest_route=/wp/v2/posts&_embed')
+      .then(data => setPosts(data.data.slice(0, 4)))
+  }, [])
 
   return (
     <div className="grid grid-cols-2 grid-rows-1 gap-1 sm:grid-cols-4 sm:gap-4">
-      {posts?.map((post, index) => (
-        <Link
-          key={String(index)}
-          href={`/artigos/${post?.slug ?? '#'}`}
-          className={`relative flex h-auto flex-col items-start justify-between transition hover:grayscale sm:h-[370px] ${divsClass[index]}`}
-        >
-          <span className="ml-4 mt-4 bg-white px-2 py-[0.1rem] font-bold sm:top-5 sm:flex">
-            Artigo
-          </span>
+      {posts && (
+        <>
+          {posts?.map((post, index) => (
+            <Link
+              key={String(index)}
+              href={`/artigos/${post?.id ?? '#'}`}
+              className={`relative flex h-auto flex-col items-start justify-between transition hover:grayscale sm:h-[370px] ${divsClass[index]}`}
+            >
+              <span className="ml-4 mt-4 bg-white px-2 py-[0.1rem] font-bold sm:top-5 sm:flex">
+                Artigo
+              </span>
 
-          <Image
-            src={
-              post?._embedded['wp:featuredmedia']?.[0]?.media_details?.sizes?.[
-                'full-width-horizontal'
-              ]?.source_url || BackgroundPlaceholder
-            }
-            alt="Image"
-            className="absolute top-0 -z-[1] object-cover"
-            width={
-              post?._embedded['wp:featuredmedia']?.[0]?.media_details?.sizes?.[
-                'full-width-horizontal'
-              ]?.width || 1024
-            }
-            height={
-              post?._embedded['wp:featuredmedia']?.[0]?.media_details?.sizes?.[
-                'full-width-horizontal'
-              ]?.height || 576
-            }
-            style={{ width: '100%', height: '100%' }}
-            placeholder={`data:image/svg+xml;base64,${toBase64(
-              shimmer(700, 475)
-            )}`}
-          />
+              <Image
+                src={
+                  post?._embedded['wp:featuredmedia']?.[0]?.media_details
+                    ?.sizes?.['full-width-horizontal']?.source_url ||
+                  BackgroundPlaceholder
+                }
+                alt="Image"
+                className="absolute top-0 -z-[1] object-cover"
+                width={
+                  post?._embedded['wp:featuredmedia']?.[0]?.media_details
+                    ?.sizes?.['full-width-horizontal']?.width || 1024
+                }
+                height={
+                  post?._embedded['wp:featuredmedia']?.[0]?.media_details
+                    ?.sizes?.['full-width-horizontal']?.height || 576
+                }
+                style={{ width: '100%', height: '100%' }}
+                placeholder={`data:image/svg+xml;base64,${toBase64(
+                  shimmer(700, 475)
+                )}`}
+              />
 
-          <h5 className="m-4 text-base font-bold text-white transition max-[425px]:text-xs sm:text-lg">
-            {post?.title?.rendered ?? 'Artigo'}
-          </h5>
-        </Link>
-      ))}
+              <h5 className="m-4 text-base font-bold text-white transition max-[425px]:text-xs sm:text-lg">
+                {post?.title?.rendered ?? 'Artigo'}
+              </h5>
+            </Link>
+          ))}
+        </>
+      )}
     </div>
   )
 }
--- src/components/ArticlesGalleryV2/index.tsx
+++ src/components/ArticlesGalleryV2/index.tsx
@@ -6,134 +6,17 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { ArticlesGallery } from '../ArticlesGallery'
 import Image from 'next/image'
 import { Spinner } from '../ui/Spinner'
-import api from '@/service/api'
 import { FilterArticles } from '../ArticlesGallery/FilterArticles'
-import LegacySiteURL from '@/common/utils/variables'
-
-export const mockArticles: any[] = [
-  {
-    id: 12515,
-    slug: 'paper-branding-trends-2023',
-    title: { rendered: 'PAPER BRANDING TRENDS 2023' },
-    _embedded: {
-      'wp:term': [[{ name: 'Downloads' }]],
-      'wp:featuredmedia': [
-        {
-          media_details: {
-            sizes: {
-              'full-width-horizontal': {
-                source_url: `${LegacySiteURL}/wp-content/uploads/2023/03/SITE-1.png`
-              }
-            }
-          }
-        }
-      ]
-    }
-  },
-  {
-    id: 10398,
-    slug: 'branding-na-cadeira-do-ceo',
-    title: { rendered: 'BRANDING NA CADEIRA DO CEO' },
-    _embedded: {
-      'wp:term': [[{ name: 'Artigos' }, { name: 'Matéria' }]],
-      'wp:featuredmedia': [
-        {
-          media_details: {
-            sizes: {
-              'full-width-horizontal': {
-                source_url: `${LegacySiteURL}/wp-content/uploads/2021/10/TIP-TALKS_21-2-site.png`
-              }
-            }
-          }
-        }
-      ]
-    }
-  },
-  {
-    id: 12420,
-    slug: 'calendario-2023/',
-    title: {
-      rendered: 'ORGANIZAÇÃO E IDEIAS EM AÇÃO: BAIXE O SEU CALENDÁRIO 2023'
-    },
-    _embedded: {
-      'wp:term': [[{ name: 'Artigos' }, { name: 'Matéria' }]],
-      'wp:featuredmedia': [
-        {
-          media_details: {
-            sizes: {
-              'full-width-horizontal': {
-                source_url: `${LegacySiteURL}/wp-content/uploads/2023/01/header_site-1.png`
-              }
-            }
-          }
-        }
-      ]
-    }
-  },
-  {
-    id: 12018,
-    slug: 'proposito-decodificado-um-olhar-renovador-sobre-um-tema-cansado/',
-    title: {
-      rendered:
-        'Propósito decodificado: um olhar renovador sobre um tema cansado'
-    },
-    _embedded: {
-      'wp:term': [[{ name: 'Artigos' }, { name: 'Downloads' }]],
-      'wp:featuredmedia': [
-        {
-          media_details: {
-            sizes: {
-              'full-width-horizontal': {
-                source_url: `${LegacySiteURL}/wp-content/uploads/2022/09/PAPER_capa_V02-1.png`
-              }
-            }
-          }
-        }
-      ]
-    }
-  },
-  {
-    id: 11171,
-    slug: 'proposito-decodificado-um-olhar-renovador-sobre-um-tema-cansado/',
-    title: { rendered: 'BRANDING BRASIL – O VALOR QUE O PAÍS GERA' },
-    _embedded: {
-      'wp:term': [[{ name: 'Materias' }]],
-      'wp:featuredmedia': [
-        {
-          media_details: {
-            sizes: {
-              'full-width-horizontal': {
-                source_url: `${LegacySiteURL}/wp-content/uploads/2022/07/BB_banner_site_1-1.png`
-              }
-            }
-          }
-        }
-      ]
-    }
-  }
-]
-
-const FilterCategories = {
-  Downloads: 137,
-  Artigos: 5,
-  Matérias: 1,
-  Todas: 0,
-  'TIP Insights': 134
-}
+import axios from 'axios'
 
 const ArticlesGalleryV2 = () => {
-  const defaultArticles = [mockArticles, [], []] as GetPostsProps[][]
+  const defaultArticles = [[]] as GetPostsProps[][]
   const [articles, setArticles] = useState({
     data: defaultArticles,
     isLoading: false
   })
 
   const [dataToFilter, setDataToFilter] = useState<GetPostsProps[]>([])
-  useEffect(() => {
-    api
-      .get(`/posts?_embed&per_page=60&page=1`)
-      .then(data => setDataToFilter(data.data))
-  }, [])
 
   const HandleGetPosts = useCallback(() => {
     try {
@@ -142,8 +25,10 @@ const ArticlesGalleryV2 = () => {
         isLoading: true
       })
 
-      api
-        .get(`/posts?_embed&per_page=5&page=${articles.data.length}`)
+      axios
+        .get(
+          ` https://web.anacouto.com.br/?rest_route=/wp/v2/posts&_embed&per_page=5&page=${articles.data.length}`
+        )
         .then(data => {
           setArticles({
             data: [...articles.data, data.data],
@@ -154,6 +39,16 @@ const ArticlesGalleryV2 = () => {
       console.log('Request to Wordpress Failed')
     }
   }, [articles.data])
+
+  useEffect(() => {
+    HandleGetPosts()
+    axios
+      .get(
+        `https://web.anacouto.com.br/?rest_route=/wp/v2/posts&_embed&per_page=60&page=1`
+      )
+      .then(data => setDataToFilter(data.data))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const handleFilter = (value: number) => {
     if (value === undefined) return
